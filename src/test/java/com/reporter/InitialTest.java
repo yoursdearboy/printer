25633e1a75e80199cd60e99e70a587a0196b655e
Kirill Voronin <yoursdearboy@gmail.com>
2020-10-12 14:40:05 +0300

    Added Java support to project and simple JUnit test

---
+++ src/test/java/com/reporter/InitialTest.java
@@ -0,0 +1,10 @@
+package com.reporter;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class InitialTest {
+    @Test public void testSomething() {
+        assertTrue("Should be true", true);
+    }
+}
