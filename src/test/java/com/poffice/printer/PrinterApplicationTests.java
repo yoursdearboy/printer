3df69590e6cb82622403bba0a64ab7077aed9497
Kirill Voronin <yoursdearboy@gmail.com>
2020-10-14 14:07:35 +0300

    Added Spring to the project

---
+++ src/test/java/com/poffice/printer/PrinterApplicationTests.java
@@ -0,0 +1,11 @@
+package com.poffice.printer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrinterApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
