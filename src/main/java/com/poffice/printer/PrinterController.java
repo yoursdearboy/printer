0d7770bea6eddb6467e7f2ca65b421971ae98944
Kirill Voronin <yoursdearboy@gmail.com>
2020-10-17 14:54:47 +0300

    Demo Docx4j usage

--- src/main/java/com/poffice/printer/PrinterController.java
+++ src/main/java/com/poffice/printer/PrinterController.java
@@ -1,5 +1,7 @@
 package com.poffice.printer;
 
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,7 +13,9 @@ import java.io.IOException;
 @Controller
 public class PrinterController {
     @RequestMapping(value = "/*", method = RequestMethod.POST)
-    public void print(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void print(HttpServletRequest request, HttpServletResponse response) throws IOException, Docx4JException {
+       WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(new java.io.File("demo.docx")); 
+ 
 	byte[] result = "Hello, World!".getBytes();
         response.getOutputStream().write(result);
 	response.setHeader("Content-Type", "plain/text");
