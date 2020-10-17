98f2b6a103131c6217859c220b3cf47918e6124a
Kirill Voronin <yoursdearboy@gmail.com>
2020-10-17 15:56:55 +0300

    Update PrinterController.java

--- src/main/java/com/poffice/printer/PrinterController.java
+++ src/main/java/com/poffice/printer/PrinterController.java
@@ -2,6 +2,7 @@ package com.poffice.printer;
 
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,13 +11,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
 
 @Controller
 public class PrinterController {
     @RequestMapping(value = "/*", method = RequestMethod.POST)
-    public void print(HttpServletRequest request, HttpServletResponse response) throws IOException, Docx4JException, URISyntaxException {
-       File file = new File(getClass().getClassLoader().getResource("/demo.docx").toURI());
+    public void print(HttpServletRequest request, HttpServletResponse response) throws IOException, Docx4JException {
+       File file = new ClassPathResource("demo.docx").toFile();
        WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(file); 
  
 	byte[] result = "Hello, World!".getBytes();
