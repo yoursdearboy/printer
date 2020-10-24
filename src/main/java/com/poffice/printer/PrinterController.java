dc0521204fc41c187add535bd6222efd7981819c
yuurrraaaa <danilkin.ur.work@gmail.com>
2020-10-24 15:49:28 +0300

    read request body + try xhtml-importer implementation

--- src/main/java/com/poffice/printer/PrinterController.java
+++ src/main/java/com/poffice/printer/PrinterController.java
@@ -2,13 +2,17 @@ package com.poffice.printer;
 
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -18,10 +22,13 @@ import java.nio.file.Path;
 
 @Controller
 public class PrinterController {
+    @Autowired
+    private PrinterService printer;
+
     @RequestMapping(value = "/*", method = RequestMethod.POST)
     public void print(HttpServletRequest request, HttpServletResponse response) throws IOException, Docx4JException, Exception {
-        PrinterService printer = new PrinterService();
-        File file = printer.print();
+        ServletInputStream reader = request.getInputStream();
+        File file = printer.print(reader);
 
         response.setContentLength((int) file.length());
         String mimeType = URLConnection.guessContentTypeFromName(file.getName());
--- src/main/java/com/poffice/printer/PrinterService.java
+++ src/main/java/com/poffice/printer/PrinterService.java
@@ -1,17 +1,28 @@
 package com.poffice.printer;
 
+import org.docx4j.convert.in.xhtml.XHTMLImporter;
+import org.docx4j.convert.in.xhtml.XHTMLImporterImpl;
 import org.docx4j.openpackaging.exceptions.InvalidFormatException;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
 
+import javax.servlet.ServletInputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
+@Service
 public class PrinterService {
-    public File print() throws InvalidFormatException, IOException, Exception {
-        File file = new ClassPathResource("demo.docx").getFile();
-        if (file == null) {
-            throw new Exception("no demo file");
-        }
-        return file;
+    public File print(ServletInputStream body) throws InvalidFormatException, IOException, Exception {
+        WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.createPackage();
+        XHTMLImporter importer = new XHTMLImporterImpl(wordMLPackage);
+        List<Object> content = importer.convert(body,"/Users/yuurrraaaa/IdeaProjects/printer/src/main/resources");
+        wordMLPackage.getMainDocumentPart().getContent().addAll(content);
+        File exportFile = new File("demo.docx");
+        wordMLPackage.save(exportFile);
+
+        return exportFile;
     }
 }
