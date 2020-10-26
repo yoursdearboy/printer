08983f4262f4880cfef50702a2ffa55d095279af
Kirill Voronin <yoursdearboy@gmail.com>
2020-10-26 12:32:05 +0300

    Added LibreOffice support

--- src/main/java/com/poffice/printer/LibreOfficePDFWriter.java
+++ src/main/java/com/poffice/printer/LibreOfficePDFWriter.java
@@ -1,7 +1,11 @@
 package com.poffice.printer;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,8 +15,19 @@ public class LibreOfficePDFWriter implements Writer {
     @Autowired
     WriterFactory writerFactory;
 
+    @Autowired
+    LibreOfficePDFConverter converter;
+
     @Override
     public void write(InputStream doc, OutputStream out) throws WriterException {
-        writerFactory.getWriter("Docx4jXHTMLWriter").write(doc, out);
+        try {
+            File tempFile = File.createTempFile("topdf", ".docx");
+            OutputStream tempFileOutputStream = new FileOutputStream(tempFile);
+            writerFactory.getWriter("Docx4jXHTMLWriter").write(doc, tempFileOutputStream);
+            File pdfFile = converter.convert(tempFile);
+            Files.copy(pdfFile.toPath(), out);
+        } catch (IOException | LibreofficeException e) {
+            throw new WriterException(e.getMessage());
+        }
     }
 }
---
+++ src/main/java/com/poffice/printer/LibreofficeException.java
@@ -0,0 +1,7 @@
+package com.poffice.printer;
+
+public class LibreofficeException extends Exception {
+    LibreofficeException(String s) {
+        super(s);
+    }
+}
